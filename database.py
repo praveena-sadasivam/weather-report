85e1a81e41d2e752fa3ecd24374182613d16ee92
praveena-sadasivam <62805270+praveena-sadasivam@users.noreply.github.com>
2020-06-29 19:26:30 +0530

    Update database.py

--- database.py
+++ database.py
@@ -27,7 +27,7 @@ def run_query(sql_query,args=[]):
 
 #for creating table
 def create_table():
-    sql_query=""" create table if not exists weather(
+    sql_query=""" create table if not exists weather_report(
         id integer primary key,
         location text,
         description text,
@@ -40,22 +40,27 @@ def create_table():
 
 #to insert new entries into table
 def insert_into_weather(location,description,temp,time):
-    sql_query="""insert into weather(location,description,temp,time) values (?,?,?,?)
+    sql_query="""insert into weather_report(location,description,temp,time) values (?,?,?,?)
     """
     run_query(sql_query,[location,description,temp,time])
     print("\n new values added into table \n")
 
 #to get all values based on location 
-def get_value(location):
-    sql_query="""select description,temp,time from weather where location = ? """
+def get_value_from_DB(location):
+    sql_query="""select description,temp,time from weather_report where location = ? """
     print("\n extracting values from table \n")
     return run_query(sql_query,[location])
 
 #to print all values from table
 def print_val():
-    sql_query="""select * from weather
+    sql_query="""select * from weather_report
     """
     return run_query(sql_query)
+#to delete old value
+def delete_record(location):
+    sql_query="""DELETE FROM weather_report WHERE location = ? """
+    run_query(sql_query,[location])
+    print("\nold value deleted\n")
 
 
 #for db connection
